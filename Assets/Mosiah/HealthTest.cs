5357ce41a2fab0bf67ee31331f7d0f3a994da6f2
MosiahF <17077225@mukilteo.wednet.edu>
2021-01-01 18:04:52 -0800

    Attempted A health bar system, will continue later

---
+++ Assets/Mosiah/HealthTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTest : MonoBehaviour
+{
+    public HealthBar healthbar1;
+    public float health = 1f;
+
+
+    private void Update()
+    {
+
+        if (health > 0)
+        {
+
+            StartCoroutine(healthbar());
+            health -= 0.1f;
+            healthbar1.SetSize(health);
+
+        }
+    }
+    IEnumerator healthbar()
+    {
+
+        yield return new WaitForSeconds(0.03f);
+
+    }
+}
