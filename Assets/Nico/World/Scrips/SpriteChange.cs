185866aab50106f9110a03406550b99e14a57a2e
MosiahF <17077225@mukilteo.wednet.edu>
2021-01-08 14:05:09 -0800

    Merge branch 'main' of https://github.com/Sawyer893/Vgd-21-q2-pm9-2.0
    Conflicts:
    	Assets/Mosiah/Sprite/NemiRight.meta
    	Assets/Nico/World/Scrips.meta

--- Assets/Nico/World/Scrips/SpriteChange.cs
+++ Assets/Nico/World/Scrips/SpriteChange.cs
@@ -11,8 +11,19 @@ public class SpriteChange : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    
+   
+
+    
+    private Dictionary<Sprite, Sprite> RunCycle = new Dictionary<Sprite, Sprite>();
+    public Sprite GetReplacementSprite(Sprite orig, string replacementFolderPath)
     {
-        
+        if (RunCycle.ContainsKey(orig)) return RunCycle[orig];
+        Sprite Idle = Resources.Load<Sprite>(replacementFolderPath + "/" + orig.name); // Or however it is you load your replacement sprites
+        RunCycle.Add(orig, Idle);
+        return Idle;
     }
 }
+
+
+
