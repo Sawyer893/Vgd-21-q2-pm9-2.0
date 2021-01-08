9bc1279d87c195bc14b9836297a1eb717b32d1f3
NicoEarley03 <1703103@mukilteo.wednet.edu>
2021-01-08 15:30:06 -0800

    Update SpriteChange.cs

--- Assets/Nico/World/Scrips/SpriteChange.cs
+++ Assets/Nico/World/Scrips/SpriteChange.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpriteChange : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
     void Start()
     {
         
